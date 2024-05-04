c7fbd35455ece0198b956057c51fa3c581f771d0
mengxain0913 <mengxian0913@gmail.com>
2024-05-04 21:06:13 +0800

    fix branch

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -11,7 +11,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { TabBarIcon } from '@/components/Themed';
 
 const ButtomTabs = createBottomTabNavigator();
-export default function TabLayout() {
+
+
+function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
@@ -59,5 +61,82 @@ export default function TabLayout() {
         />
       </ButtomTabs.Navigator>
     </NavigationContainer>
-  );
+=======
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+
+import Colors from "@/constants/Colors";
+import { useColorScheme } from "@/components/useColorScheme";
+import { useClientOnlyValue } from "@/components/useClientOnlyValue";
+
+interface Props {
+  name: React.ComponentProps<typeof Ionicons>["name"];
+  color: string;
 }
+
+// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
+const TabBarIcon = ({ name, color }: Props) => {
+  return (
+    <Ionicons size={28} style={{ marginBottom: 0 }} name={name} color={color} />
+  );
+};
+
+const TabLayout = () => {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        headerShown: useClientOnlyValue(false, false),
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="home-outline" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="order"
+        options={{
+          title: "Order Info",
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="receipt" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="new"
+        options={{
+          title: "New Product",
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="add-circle-outline" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="analysis"
+        options={{
+          title: "Analysis",
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="analytics-outline" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="admin"
+        options={{
+          title: "Admin",
+          tabBarIcon: ({ color }) => <TabBarIcon name="person" color={color} />,
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default TabLayout;
